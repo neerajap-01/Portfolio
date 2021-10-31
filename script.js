e93439e278f505175291cdf3f320ad6b884fd228
Neeraj <neerajap.01@gmail.com>
2021-10-31 16:27:03 +0530

    init

--- script.js
+++ script.js
@@ -30,6 +30,18 @@ $(document).ready(function() {
       $('.menu-btn i').toggleClass("active");
     });
 
+    //readmore popup
+    const open = document.getElementById('open');
+    const modal_container = document.getElementById('modal-container');
+    const close = document.getElementById('close');
+
+    open.addEventListener('click', () => {
+      modal_container.classList.add('show');
+    });
+    close.addEventListener('click', () => {
+      modal_container.classList.remove('show');
+    });
+
     // typing animation script
     var typed = new Typed(".typing",{
       strings: ["Developer.","Freelancer.","Tester.","Designer.","Learner."],
