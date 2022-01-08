ad3d455999aa5acedbbd5580c46c0ec0a9c30376
Neeraj <neerajap.01@gmail.com>
2022-01-08 14:30:23 +0530

    Hire me Popup

--- script.js
+++ script.js
@@ -120,3 +120,18 @@ function init() {
 }
 
 init();
+
+// Hire me Popup menu
+const section = document.querySelector('.section'),
+      hireBtn = document.querySelector('#hireBtn'),
+      closeBtn = document.querySelectorAll('#close_hire');
+
+    hireBtn.addEventListener("click", () => {
+      section.classList.add("show");
+    });
+
+    closeBtn.forEach(cBtn => {
+      cBtn.addEventListener("click", () => {
+        section.classList.remove("show");
+      });
+    });
