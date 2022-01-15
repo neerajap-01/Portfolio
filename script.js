cf25210e322bafd4a2aeb81ae371e019a5146f4f
Neeraj <neerajap.01@gmail.com>
2022-01-15 14:38:19 +0530

    Till share menu completed

--- script.js
+++ script.js
@@ -96,42 +96,6 @@ $(document).ready(function() {
         }
       }
     });
-
-    // Custom Right Click
-  const contextMenu = document.querySelector(".wrapper1",".about"),
-  shareMenu = contextMenu.querySelector(".share-menu");
-
-  document.addEventListener("contextmenu", e => {
-    e.preventDefault(); // prevent default context menu of the browser
-
-    let x = e.offsetX, y = e.offsetY,
-    winWidth = window.innerWidth
-    winHeight = window.innerHeight,
-    cmWidth = contextMenu.offsetWidth,
-    cmHeight = contextMenu.offsetHeight;
-
-    // if x is greater than window width - contextMenu width - shareMenu width
-    // when show the share menu to the left else show it to the right
-    if(x > (winWidth - cmWidth - shareMenu.offsetWidth)) {
-      shareMenu.style.left ="-200px";
-    } else {
-      shareMenu.style.left ="";
-      shareMenu.style.right ="-200px";
-    }
-
-    // if x is greater than window width - contextMenu width then set the x value
-    // to window width - contextMenu width else set x to the offsetX. Similarly, to y.
-    x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
-    y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
-
-    contextMenu.style.left = `${x}px`;
-    contextMenu.style.top = `${y}px`;
-    contextMenu.style.visibility = "visible";
-  });
-
-  //hide the context menu on click
-  document.addEventListener("click", () => contextMenu.style.visibility = "hidden");
-
 });
 
 //Sharing Social Buttons
@@ -189,3 +153,39 @@ closeBtn.forEach(cBtn => {
     contact.classList.remove('blur');
   });
 });
+
+    // Custom Right Click
+    const contextMenu = document.querySelector(".wrapper1",".about"),
+    shareMenu = contextMenu.querySelector(".share-menu");
+  
+    document.addEventListener("contextmenu", e => {
+      e.preventDefault(); // prevent default context menu of the browser
+  
+      let x = e.pageX, y = e.pageY,
+      winWidth = window.innerWidth
+      winHeight = window.offsetHeight,
+      cmWidth = contextMenu.offsetWidth,
+      cmHeight = contextMenu.offsetHeight;
+  
+      // if x is greater than window width - contextMenu width - shareMenu width
+      // then show the share menu to the left else show it to the right
+      if(x > (winWidth - cmWidth - shareMenu.offsetWidth)) {
+        shareMenu.style.left ="-200px";
+      } else {
+        shareMenu.style.left ="";
+        shareMenu.style.right ="-200px";
+      }
+  
+      // if x is greater than window width - contextMenu width then set the x value
+      // to window width - contextMenu width else set x to the offsetX. Similarly, to y.
+      x = x > winWidth - cmWidth ? winWidth - cmWidth : x;
+      y = y > winHeight - cmHeight ? winHeight - cmHeight : y;
+  
+      contextMenu.style.left = `${x}px`;
+      contextMenu.style.top = `${y}px`;
+      contextMenu.style.visibility = "visible";
+    });
+  
+    //hide the context menu on click
+    document.addEventListener("click", () => contextMenu.style.visibility = "hidden");
+  
